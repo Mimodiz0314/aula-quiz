7619daf59c409f54f02c0982bbe3f6e93e224d5e
Mimodiz0314 <tu-correo@gmail.com>
2026-05-20 22:14:38 -0500

    fix: unregister orphaned service workers

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,15 @@ import App from './App.jsx';
 import { ErrorBoundary } from './ErrorBoundary.jsx';
 import './index.css';
 
+// Eliminar cualquier Service Worker huérfano de instalaciones PWA anteriores
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
